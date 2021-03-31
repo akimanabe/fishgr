a19012bbabb0790f9e884d282cae31c54e4f529a
Akihiro Manabe <amanabe@affrc.go.jp>
2021-03-31 17:02:33 +0900

    :sparkles: Add vb

---
+++ R/vb.R
@@ -0,0 +1,15 @@
+#' von Bertalanffy growth function
+#'
+#' @param age numerical age
+#' @param ps vectorized parameters in order of c(Linf, K, t0)
+#'
+#' @return numerical or vector
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' vb(1, c(300, 0.6, 0))
+#' }
+vb <- function(age, ps) {
+  ps[1] * (1 - exp( - ps[2] * (age - ps[3])))
+}
