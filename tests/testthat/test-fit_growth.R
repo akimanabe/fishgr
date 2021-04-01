f09d76a4cd43f4a7de6f1623afc5fa05bfb5f73a
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-01 15:03:12 +0900

    :sparkles: Add fit_vb
    :white_check_mark: Add tests for fit_vb
    :arrow_up: Add dependencies

--- tests/testthat/test-fit_growth.R
+++ tests/testthat/test-fit_growth.R
@@ -1,13 +1,16 @@
 context("fit growth")
 
 test_that("function exists", {
-  expect_is(fit_growth, "function")
+  expect_is(fit_vb, "function")
 })
 
 test_that("function estimates parameters", {
-  sampledata <- vb(seq(0, 5, 0.1), ps = c(300, 0.6, 0))
+  foo <-
+    tibble::tibble(Age = seq(0, 5, 0.1),
+                   Length = vb(seq(0, 5, 0.1),
+                               ps = c(300, 0.6, 0)))
 
-  expect_is(fit_growth(sampledata,
+  expect_is(fit_vb(foo,
                        ps = c(200, 0.8, 0)),
             "data.frame")
 })
