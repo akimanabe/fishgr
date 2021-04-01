5237000ef44397240e376ea4c6bcb56c1981c100
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-01 14:06:01 +0900

    :white_check_mark: Add test for fit_growth

--- tests/testthat/test-fit_growth.R
+++ tests/testthat/test-fit_growth.R
@@ -5,5 +5,9 @@ test_that("function exists", {
 })
 
 test_that("function estimates parameters", {
-  expect_is(fit_growth(foo, ps = c(300, 0.6, 0), model = "vb"), "data.frame")
+  sampledata <- vb(seq(0, 5, 0.1), ps = c(300, 0.6, 0))
+
+  expect_is(fit_growth(sampledata,
+                       ps = c(200, 0.8, 0)),
+            "data.frame")
 })
