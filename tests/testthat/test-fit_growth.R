70df064a8612e336786a05643080da7b245e05c1
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-01 16:53:45 +0900

    :white_check_mark: Add test for fit ext_vb

--- tests/testthat/test-fit_growth.R
+++ tests/testthat/test-fit_growth.R
@@ -1,4 +1,4 @@
-context("fit growth")
+context("fit vb")
 
 test_that("function exists", {
   expect_is(fit_vb, "function")
@@ -12,5 +12,22 @@ test_that("function estimates parameters", {
 
   expect_is(fit_vb(foo,
                        ps = c(200, 0.8, 0)),
+            "data.frame")
+})
+
+context("fit ext_vb")
+
+test_that("function exits", {
+  expect_is(fit_ext_vb, "function")
+})
+
+test_that("function estimates parameters", {
+  foo <-
+    tibble::tibble(Age = seq(0, 5, 0.1),
+                   Length = vb(seq(0, 5, 0.1),
+                               ps = c(300, 0.6, 0)))
+
+  expect_is(fit_ext_vb(foo,
+                       ps = c(200, 0.6, 0.4, 2, 0)),
             "data.frame")
 })
