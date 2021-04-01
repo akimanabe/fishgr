c1efd10affbe5b8b180756523675163d9d3106b7
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-01 16:37:00 +0900

    :sparkles: Add exst_vb #25

--- R/vb.R
+++ R/vb.R
@@ -13,3 +13,24 @@
 vb <- function(age, ps) {
   ps[1] * (1 - exp(- ps[2] * (age - ps[3])))
 }
+
+#' extended von Bertalanffy growth function
+#'
+#' @param age numerical age
+#' @param ps vectorized parameters in order of c(Linf, K1, K2, tm, t0)
+#'
+#' @return numerical or vector
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ext_vb(seq(0,5, 0.5), ps = c(300, 0.6, 0.4, 2, 0))
+#' }
+ext_vb <- function(age, ps) {
+  x <-
+    ifelse(age < ps[4],
+           - ps[2] * (age - ps[5]),
+           - ps[2] * (ps[4] - ps[5]) - ps[3] * (age - ps[4]))
+
+  ps[1] * (1 - exp(x))
+}
