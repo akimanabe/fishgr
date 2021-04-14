dcfc8731d95986b7d95eb23e6121f745ad38770b
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-14 15:09:27 +0900

    :white_check_mark: Update test for ext_vb

--- tests/testthat/test-vb.R
+++ tests/testthat/test-vb.R
@@ -23,9 +23,15 @@ test_that("function exists", {
 test_that("function estimates Length at age", {
   length_2 <- ext_vb(2, ps = c(300, 0.6, 0.4, 2, 0))
   expect_equal(length_2, 209.6417, tolerance = 0.0001)
+
+  length_3 <- ext_vb(3, ps = c(300, 0.6, 0.4, 2, 0))
+  expect_equal(length_3, 236.9592, tolerance = 0.0001)
 })
 
 test_that("setorigin set t_0 as 0 for extVB", {
-  length_0 <- ext_vb(age = 2, ps = c(300, 0.5, 0.6, 3), setorigin = TRUE)
-  expect_equal(length_0, 189.6362, tolerance = 0.0001)
+  length_0_2 <- ext_vb(age = 2, ps = c(300, 0.5, 0.6, 3), setorigin = TRUE)
+  expect_equal(length_0_2, 189.6362, tolerance = 0.0001)
+
+  length_0_4 <- ext_vb(age = 4, ps = c(300, 0.5, 0.6, 3), setorigin = TRUE)
+  expect_equal(length_0_4, 245.1949, tolerance = 0.0001)
 })
