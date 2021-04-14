acd33d6fdaa3d122acb17b5e71978cb3dc4d52c6
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-14 15:13:25 +0900

    :bug: Fix ext_vb function

--- R/vb.R
+++ R/vb.R
@@ -29,7 +29,7 @@ vb <- function(age, ps, setorigin = FALSE) {
 #' extended von Bertalanffy growth function
 #'
 #' @param age numerical age
-#' @param ps vectorized parameters in order of c(Linf, K1, K2, tm, t0)
+#' @param ps vectorized parameters in order of c(Linf, K, v, tm, t0)
 #' @param setorigin constrain t_0 to 0 when TRUE
 #'
 #' @return numerical or vector
@@ -44,7 +44,8 @@ ext_vb <- function(age, ps, setorigin = FALSE) {
     x <-
       ifelse(age < ps[4],
              - ps[2] * (age - ps[5]),
-             - ps[2] * (ps[4] - ps[5]) - ps[3] * (age - ps[4]))
+             - ps[2] * ((age - ps[5]) - ps[3] * (age - ps[4]))
+      )
 
     estim_length <- ps[1] * (1 - exp(x))
   }
@@ -53,7 +54,8 @@ ext_vb <- function(age, ps, setorigin = FALSE) {
     x <-
       ifelse(age < ps[4],
              - ps[2] * (age - 0),
-             - ps[2] * (ps[4] - 0) - ps[3] * (age - ps[4]))
+             - ps[2] * ((age - 0) - ps[3] * (age - ps[4]))
+      )
 
     estim_length <- ps[1] * (1 - exp(x))
   }
